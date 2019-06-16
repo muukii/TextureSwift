b7664ab37961d8b6b442e71c9e90e7d0a7b43920
Muukii <muukii.app@gmail.com>
2019-06-16 19:30:31 +0900

    Init

---
+++ Example/Pods/Target Support Files/SpecBuilder/SpecBuilder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SpecBuilder : NSObject
+@end
+@implementation PodsDummy_SpecBuilder
+@end
---
+++ SpecBuilder/Classes/SpecBuilder.swift
@@ -0,0 +1,172 @@
+import AsyncDisplayKit
+
+public protocol _ASLayoutElementType {
+  
+  func make() -> [ASLayoutElement]
+}
+
+@_functionBuilder public struct ASLayoutSpecBuilder {
+  
+  public static func buildBlock() -> AS.EmptyLayout {
+    AS.EmptyLayout()
+  }
+  
+  public static func buildBlock<Content>(_ content: Content) -> Content where Content : _ASLayoutElementType {
+    content
+  }
+  
+  public static func buildBlock(_ content: _ASLayoutElementType...) -> AS.MultiLayout {
+    AS.MultiLayout(content)
+  }
+  
+  public static func buildIf<Content>(_ content: Content?) -> Content? where Content : _ASLayoutElementType  {
+    content
+  }
+  
+  public static func buildEither<TrueContent, FalseContent>(first: TrueContent) -> ASConditionalLayout<TrueContent, FalseContent> {
+    .init(trueContent: first)
+  }
+  
+  public static func buildEither<TrueContent, FalseContent>(second: FalseContent) -> ASConditionalLayout<TrueContent, FalseContent> {
+    .init(falseContent: second)
+  }
+}
+
+extension ASDisplayNode : _ASLayoutElementType {
+  public func make() -> [ASLayoutElement] {
+    [self]
+  }
+}
+
+public struct ASConditionalLayout<TrueContent, FalseContent> : _ASLayoutElementType where TrueContent : _ASLayoutElementType, FalseContent : _ASLayoutElementType {
+  
+  let content: [ASLayoutElement]
+  
+  init(trueContent: TrueContent) {
+    self.content = trueContent.make()
+  }
+  
+  init(falseContent: FalseContent) {
+   self.content = falseContent.make()
+  }
+  
+  public func make() -> [ASLayoutElement] {
+    content
+  }
+}
+
+public enum AS {
+  
+  public final class LayoutSpec<Content> : ASWrapperLayoutSpec where Content : _ASLayoutElementType {
+    
+    public init(@ASLayoutSpecBuilder _ content: () -> Content) {
+      super.init(layoutElements: content().make())
+    }
+  }
+  
+  public struct MultiLayout : _ASLayoutElementType {
+    
+    public let elements: [_ASLayoutElementType?]
+    
+    init(_ elements: [_ASLayoutElementType?]) {
+      self.elements = elements
+    }
+    
+    public func make() -> [ASLayoutElement] {
+      elements.compactMap { $0 }.flatMap { $0.make() }
+    }
+  }
+  
+  public struct EmptyLayout : _ASLayoutElementType {
+    
+    public func make() -> [ASLayoutElement] {
+      [ASLayoutSpec()]
+    }
+  }
+  
+  public struct VStack<Content> : _ASLayoutElementType where Content : _ASLayoutElementType {
+    
+    public let spacing: CGFloat
+    public let justifyContent: ASStackLayoutJustifyContent
+    public let alignItems: ASStackLayoutAlignItems
+    public let childlen: Content
+    
+    public init(
+      spacing: CGFloat = 0,
+      justifyContent: ASStackLayoutJustifyContent = .start,
+      alignItems: ASStackLayoutAlignItems = .start,
+      @ASLayoutSpecBuilder content: () -> Content
+      ) {
+      
+      self.spacing = spacing
+      self.justifyContent = justifyContent
+      self.alignItems = alignItems
+      self.childlen = content()
+      
+    }
+    
+    public func make() -> [ASLayoutElement] {
+      [
+        ASStackLayoutSpec(
+          direction: .vertical,
+          spacing: spacing,
+          justifyContent: justifyContent,
+          alignItems: alignItems,
+          children: childlen.make()
+        )
+      ]
+    }
+  }
+  
+  public struct HStack<Content> : _ASLayoutElementType where Content : _ASLayoutElementType {
+    
+    public let spacing: CGFloat
+    public let justifyContent: ASStackLayoutJustifyContent
+    public let alignItems: ASStackLayoutAlignItems
+    public let childlen: Content
+    
+    public init(
+      spacing: CGFloat = 0,
+      justifyContent: ASStackLayoutJustifyContent = .start,
+      alignItems: ASStackLayoutAlignItems = .start,
+      @ASLayoutSpecBuilder content: () -> Content
+      ) {
+      
+      self.spacing = spacing
+      self.justifyContent = justifyContent
+      self.alignItems = alignItems
+      self.childlen = content()
+      
+    }
+    
+    public func make() -> [ASLayoutElement] {
+      [
+        ASStackLayoutSpec(
+          direction: .horizontal,
+          spacing: spacing,
+          justifyContent: justifyContent,
+          alignItems: alignItems,
+          children: childlen.make()
+        )
+      ]
+    }
+    
+  }
+  
+  public struct Inset<Content> : _ASLayoutElementType where Content : _ASLayoutElementType {
+    
+    public let content: Content
+    public let insets: UIEdgeInsets
+    
+    public init(insets: UIEdgeInsets, content: () -> Content) {
+      self.content = content()
+      self.insets = insets
+    }
+    
+    public func make() -> [ASLayoutElement] {
+      [ASInsetLayoutSpec(insets: insets, child: content.make().first!)]
+    }
+  }
+
+  
+}
